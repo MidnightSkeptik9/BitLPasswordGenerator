2e6137cc0fdc35f7f7578f0fb739bc3c53f7f586
MidnightSkeptik9 <142453990+MidnightSkeptik9@users.noreply.github.com>
2023-09-17 18:39:27 -0400

    Update main.py, making the code simpler

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-import pyautogui # not used but I will leave it for your use if you so choose
+# not used but I will leave it for your use if you so choose
 
 # define a list of characters that can appear within a bitlocker
 # recovery password
@@ -9,26 +9,12 @@ characterList = list(characters)
 # define a funtion to make a password as a list with specific formatting
 # refer to Model 1 within README.md to see an example list
 def makePassword(): # *** returns a list *** #
-    password = []
-    for _ in range(0, 12):
-        nestedList = []
-        for _ in range(0, 4):
-            char = random.choice(characterList)
-            nestedList.append(char)
-        password.append(nestedList)
+    password = ""
+    for _ in range(0, 48):
+        password += str(random.choice(characterList))
     return password
 
-# define a function to loop through the list created in the function
-# above that formats it as Model 2 in README.md is formatted
-def formatAsString(listObject): # *** returns a string *** #
-    passwordString = ''
-    for x in range(0, 8):
-        for y in range(0, 4):
-            char = listObject[x][y]
-            passwordString += str(char)
-    # this creates an invalid password with a hyphen at the end
-    # so we use removesuffix() to get rid of 
-    return passwordString
+print(len(makePassword()))
 
 # making a password:
 # password = makePassword()
